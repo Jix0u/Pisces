ff7eecc1209230d3f577cc9a6da1aac7ad8a0317
Jesse <83730324+dev-jesse@users.noreply.github.com>
2024-02-17 11:06:51 -0500

    removed tailwind from file + restart from scratch frontend

---
+++ frontend/app/fonts.js
@@ -0,0 +1,11 @@
+import { Inter, Roboto_Condensed } from 'next/font/google'
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const roboto_condensed = Roboto_Condensed({
+  subsets: ['latin'],
+  display: 'swap',
+})
